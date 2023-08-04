9b734ce76959b70962d1cc012ecb5ec09854d9c3
Kevin Huang <khuang891@gmail.com>
2023-08-04 15:44:13 +1000

    made front end

---
+++ src/utils/goodreads.ts
@@ -0,0 +1,9 @@
+export default async function fetchData() {
+  const response = await fetch(
+    "https://www.googleapis.com/books/v1/volumes?q=dune&max_results=5"
+  );
+  const movies = await response.json();
+  console.log(movies);
+}
+
+void fetchData();
